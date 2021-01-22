5cd0ff779e4f8a1c4070129fcb0fe2de9bb43269
Mohammad  Boustta <moboustt@e2r9p4.1337.ma>
2021-01-22 11:46:58 +0100

    Update setUp script to install vim-plug, nvim and python3

--- setUp.sh
+++ setUp.sh
@@ -12,7 +12,25 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
 echo 'source  ~/powerlevel9k/powerlevel9k.zsh-theme' >> ~/.zshrc
 
-# Install Nerd-Fonts & brew cask
+# Install Vim-Plug for vim/nvim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Install Nerd-Fonts & brew cask
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /Users/moboustt/.oh-my-zsh//custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions /Users/moboustt/.oh-my-zsh//custom/plugins/zsh-autosuggestions
+
+# Install nvim
+brew install nvim
+mkdir -p ~/.config/nvim
+
+
+# Some plugins require this. It's important.
+brew install python3
+/usr/local/bin/pip install neovim
+/usr/local/bin/pip install --upgrade neovim
+
+
